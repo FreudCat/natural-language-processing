553c82ef50d7b98c7dc6a1353c62e25b46c0924b
= <anguyen031@gmail.com>
2021-05-09 15:43:54 -0700

    Populate browser with sentiment

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -1,12 +1,22 @@
 export const showData = (data) => {
   console.log(data);
 
+  const resultsHolder = document.getElementById("result-holder");
+
   let sentimentObj = {
     agreement: data.agreement,
     subjectivity: data.subjectivity,
     irony: data.irony,
     confidence:data.confidence
   };
-
   console.log(sentimentObj);
+
+  for (let item in sentimentObj) {
+    console.log(item);
+    let newP = document.createElement("p");
+    newP.id = item; 
+    newP.innerHTML = sentimentObj[item];
+    console.log(newP);  
+    resultsHolder.appendChild(newP);
+  }
 }
