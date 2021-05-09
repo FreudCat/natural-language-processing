5c6d1729f04f368b1dd1fa1b27831f485cd89a0b
= <anguyen031@gmail.com>
2021-05-09 15:31:44 -0700

    Create sentiment obj to store data

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -1,8 +1,12 @@
 export const showData = (data) => {
-  let agreement = data.agreement;
-  let subjectivity = data.subjectivity;
-  let irony = data.irony;
-  let confidence = data.confidence;
+  console.log(data);
 
-  console.log(agreement, subjectivity, irony, confidence);
+  let sentimentObj = {
+    agreement: data.agreement,
+    subjectivity: data.subjectivity,
+    irony: data.irony,
+    confidence:data.confidence
+  };
+
+  console.log(sentimentObj);
 }
